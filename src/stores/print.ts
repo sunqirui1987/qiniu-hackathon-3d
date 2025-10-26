63b2e9c8fe953aa998bc4ac4aa1ff1181b74c22d
sun qirui <suiqirui1987@qq.com>
2025-10-26 14:49:43 +0800

    连接打印机

--- src/stores/print.ts
+++ src/stores/print.ts
@@ -33,6 +33,7 @@ function saveQueueToStorage(queue: PrintJob[]): void {
 export const usePrintStore = defineStore('print', () => {
   const printQueue = ref<PrintJob[]>(loadQueueFromStorage())
   const currentPrintJob = ref<PrintJob | null>(null)
+  const bambuConnected = ref(false)
 
   watch(
     printQueue,
@@ -94,9 +95,14 @@ export const usePrintStore = defineStore('print', () => {
     return printQueue.value.filter((j) => j.status === 'printing')
   }
 
+  function setBambuConnected(connected: boolean) {
+    bambuConnected.value = connected
+  }
+
   return {
     printQueue,
     currentPrintJob,
+    bambuConnected,
     addPrintJob,
     removePrintJob,
     updateJobStatus,
@@ -106,5 +112,6 @@ export const usePrintStore = defineStore('print', () => {
     getJobById,
     getPendingJobs,
     getActiveJobs,
+    setBambuConnected,
   }
 })
